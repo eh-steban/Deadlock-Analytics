adfddb0d11c0acd314b86b6eccadf32fecd03d78
Steven <sfrodriguez86@gmail.com>
2025-08-28 21:09:41 -0600

    Start styling Login.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,21 @@ import MatchAnalysis from "./components/matchAnalysis/MatchAnalysis";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className='App flex min-h-screen'>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/profile/:steam_id" element={<ProfilePage />} />
-          <Route path="/match/analysis/:match_id" element={<MatchAnalysis />} />
+          <Route
+            path='/'
+            element={<Login />}
+          />
+          <Route
+            path='/profile/:steam_id'
+            element={<ProfilePage />}
+          />
+          <Route
+            path='/match/analysis/:match_id'
+            element={<MatchAnalysis />}
+          />
         </Routes>
       </Router>
     </div>
--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import steamLoginImg from '../../assets/steam-button-vertical.png';
+import steamLoginImg from "../../assets/steam-button-vertical.png";
 
 const backendDomain = import.meta.env.VITE_BACKEND_DOMAIN || "domain";
 
 const Login: React.FC = () => (
-  <div className="mt-10 flex flex-col items-center gap-4">
+  <div className='m-auto mt-10 flex w-full max-w-md flex-col items-center justify-center gap-15 rounded-md border-2 border-stone-500 p-6 shadow-lg inset-shadow-sm/10 shadow-lime-600/50 md:mt-auto lg:h-60 lg:w-1/3 lg:flex-row'>
+    <div className='text-center'>
+      <h3 className='underline decoration-purple-500 decoration-3'>
+        dashJump.gg
+      </h3>
+      <p className='m-0'>Welcome!</p>
+    </div>
     <a
       href={`http://${backendDomain}/auth/login`}
-      className="inline-flex items-center gap-3 px-6 py-3 bg-indigo-600 text-white rounded-lg shadow hover:bg-indigo-700 transition-colors"
+      className='inline-flex items-center justify-center'
     >
-      <img src={steamLoginImg} alt="Steam logo" className="h-10 w-auto" />
+      <img
+        src={steamLoginImg}
+        alt='Steam logo'
+        className='h-10 w-auto sm:h-12 lg:h-14'
+      />
     </a>
   </div>
 );
