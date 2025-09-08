af2b4d18a884765b54be533ab2b9df25f56083e5
Steven <sfrodriguez86@gmail.com>
2025-09-08 15:15:33 -0600

    Fix login page styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,24 +7,22 @@ import MatchAnalysis from "./components/matchAnalysis/MatchAnalysis";
 
 const App: React.FC = () => {
   return (
-    <div className='App flex min-h-screen'>
-      <Router>
-        <Routes>
-          <Route
-            path='/'
-            element={<Login />}
-          />
-          <Route
-            path='/profile/:steam_id'
-            element={<ProfilePage />}
-          />
-          <Route
-            path='/match/analysis/:match_id'
-            element={<MatchAnalysis />}
-          />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={<Login />}
+        />
+        <Route
+          path='/profile/:steam_id'
+          element={<ProfilePage />}
+        />
+        <Route
+          path='/match/analysis/:match_id'
+          element={<MatchAnalysis />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -4,23 +4,27 @@ import steamLoginImg from "../../assets/steam-button-vertical.png";
 const backendDomain = import.meta.env.VITE_BACKEND_DOMAIN || "domain";
 
 const Login: React.FC = () => (
-  <div className='m-auto mt-10 flex w-full max-w-md flex-col items-center justify-center gap-15 rounded-md border-2 border-stone-500 p-6 shadow-lg inset-shadow-sm/10 shadow-lime-600/50 md:mt-auto lg:h-60 lg:w-1/3 lg:flex-row'>
-    <div className='text-center'>
-      <h3 className='underline decoration-purple-500 decoration-3'>
-        dashJump.gg
-      </h3>
-      <p className='m-0'>Welcome!</p>
+  <div className='flex h-screen w-full items-center justify-center lg:flex-row'>
+    <div className='max-w-md rounded-md border-2 border-stone-500 p-6 shadow-lg inset-shadow-sm/10 shadow-lime-600/50 lg:h-60 lg:w-1/3'>
+      <div className='flex h-full items-center justify-center gap-15 lg:flex-row lg:text-left'>
+        <div>
+          <h3 className='underline decoration-purple-500 decoration-3'>
+            dashJump.gg
+          </h3>
+          <p className='m-0 text-center'>Welcome!</p>
+        </div>
+        <a
+          href={`http://${backendDomain}/auth/login`}
+          className='mt-4 lg:mt-0'
+        >
+          <img
+            src={steamLoginImg}
+            alt='Steam logo'
+            className='h-10 w-auto sm:h-12 lg:h-14'
+          />
+        </a>
+      </div>
     </div>
-    <a
-      href={`http://${backendDomain}/auth/login`}
-      className='inline-flex items-center justify-center'
-    >
-      <img
-        src={steamLoginImg}
-        alt='Steam logo'
-        className='h-10 w-auto sm:h-12 lg:h-14'
-      />
-    </a>
   </div>
 );
 
